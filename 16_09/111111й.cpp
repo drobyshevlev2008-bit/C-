f6ae5ba933d429c373493cde0f01f59b69e9ba50
drobyshevlev2008-bit <drobyshev.lev2008@gmail.com>
2025-10-16 14:33:03 +0900

    Add files via upload

---
+++ 16_09/111111й.cpp
@@ -0,0 +1,44 @@
+﻿//Задание 1
+// #include <iostream>
+//
+//int main() {
+//    std::cout << "Вы помните,\n"
+//        "Вы всё, конечно, помните,\n"
+//        "Как вечер шелестел мелано\n"
+//        "И осень тихою вдовой\n"
+//        "Шагала в даль туманную...\n\n"
+//        "Автор: Сергей Есенин\n";
+//
+//    return 0;
+//}
+
+//Задание 2
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "Every hunter wants to know where the pheasant dwells:\n"
+//        << "\tRed,\n"
+//        << "\t\tOrange,\n"
+//        << "\t\t\tYellow,\n"
+//        << "\t\t\t\tGreen,\n"
+//        << "\t\t\t\t\tBlue,\n"
+//        << "\t\t\t\t\t\tIndigo,\n"
+//        << "\t\t\t\t\t\t\tViolet.\n";
+//
+//    return 0;
+//}
+
+//Задание 3
+//#include <iostream>
+//
+//int main() {
+//    std::cout << "Продаётся отличный велосипед!\n"
+//        << "Модель: Stels Navigator 310\n"
+//        << "Год выпуска: 2022\n"
+//        << "Цвет: синий\n"
+//        << "Цена: 15 тысяч рублей\n"
+//        << "Контактный телефон: +7 (900) 123-45-67\n"
+//        << "Торговля возможна!";
+//
+//    return 0;
+//}
