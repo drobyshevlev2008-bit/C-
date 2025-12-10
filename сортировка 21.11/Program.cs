de8d3ffce3995a3d61bcdc364b55423ba28d9628
drobyshevlev2008-bit <drobyshev.lev2008@gmail.com>
2025-12-10 14:08:55 +0900

    Add files via upload

---
+++ сортировка 21.11/Program.cs
@@ -0,0 +1,168 @@
+﻿//1
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+//2
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+//3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[10];
+    float sum = 0;
+
+    cout << "Enter 10 marks: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> marks[i];
+        sum += marks[i];
+    }
+
+    float average = sum / 10;
+
+    cout << "Average mark: " << average << endl;
+
+    if (average < 10.7)
+    {
+        cout << "Student is expelled." << endl;
+    }
+    else
+    {
+        cout << "Student is not expelled." << endl;
+    }
+
+    return 0;
+}
+//4
+#include <iostream>
+using namespace std;
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+
+        int pi = i + 1;
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+//5
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90, 10, 5, 3 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int twoThirds = (2 * n) / 3;
+
+    sort(arr, arr + twoThirds);
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
