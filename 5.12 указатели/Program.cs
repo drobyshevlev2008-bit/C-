0ef9fac7595148b7873fa7f2006e49708dea2fd0
drobyshevlev2008-bit <drobyshev.lev2008@gmail.com>
2025-12-12 13:45:18 +0900

    Add files via upload

---
+++ 5.12 указатели/Program.cs
@@ -0,0 +1,109 @@
+﻿//1
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cout << "Введите размер массива A (M): ";
+    cin >> M;
+    cout << "Введите размер массива B (N): ";
+    cin >> N;
+
+    vector<int> A(M);
+    vector<int> B(N);
+
+    cout << "Введите элементы массива A: ";
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> A[i];
+    }
+
+    cout << "Введите элементы массива B: ";
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> B[i];
+    }
+
+    set<int> result;
+
+    // Добавляем элементы из A, которые не входят в B
+    for (int i = 0; i < M; ++i)
+    {
+        if (find(B.begin(), B.end(), A[i]) == B.end())
+        {
+            result.insert(A[i]);
+        }
+    }
+
+    // Выводим результат
+    cout << "Результат: ";
+    for (const auto&elem : result) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+//2
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    int M, N;
+    cout << "Введите размер массива A (M): ";
+    cin >> M;
+    cout << "Введите размер массива B (N): ";
+    cin >> N;
+
+    vector<int> A(M);
+    vector<int> B(N);
+
+    cout << "Введите элементы массива A: ";
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> A[i];
+    }
+
+    cout << "Введите элементы массива B: ";
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> B[i];
+    }
+
+    set<int> result;
+
+    // Добавляем элементы из A, которые не входят в B
+    for (int i = 0; i < M; ++i)
+    {
+        if (find(B.begin(), B.end(), A[i]) == B.end())
+        {
+            result.insert(A[i]);
+        }
+    }
+
+    // Добавляем элементы из B, которые не входят в A
+    for (int i = 0; i < N; ++i)
+    {
+        if (find(A.begin(), A.end(), B[i]) == A.end())
+        {
+            result.insert(B[i]);
+        }
+    }
+
+    // Выводим результат
+    cout << "Результат: ";
+    for (const auto&elem : result) {
+        cout << elem << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
