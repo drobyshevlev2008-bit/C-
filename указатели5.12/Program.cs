49f7a87904a6a3dbe6f087c02ace0bc5e5ba8ae9
drobyshevlev2008-bit <drobyshev.lev2008@gmail.com>
2025-12-20 18:16:53 +0900

    Add files via upload

---
+++ указатели5.12/Program.cs
@@ -0,0 +1,97 @@
+﻿//1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << "Наибольшее число: " << a << endl;
+    }
+    else
+    {
+        cout << "Наибольшее число: " << b << endl;
+    }
+    return 0;
+}
+//2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Введите число: ";
+    cin >> num;
+    if (num > 0)
+    {
+        cout << "Число положительное" << endl;
+    }
+    else if (num < 0)
+    {
+        cout << "Число отрицательное" << endl;
+    }
+    else
+    {
+        cout << "Число равно нулю" << endl;
+    }
+    return 0;
+}
+//3
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, temp;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "После обмена:" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    return 0;
+}
+//4
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    char op;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    cout << "Введите операцию (+, -, *, /): ";
+    cin >> op;
+    switch (op)
+    {
+        case '+':
+            cout << "Результат: " << a + b << endl;
+            break;
+        case '-':
+            cout << "Результат: " << a - b << endl;
+            break;
+        case '*':
+            cout << "Результат: " << a * b << endl;
+            break;
+        case '/':
+            if (b != 0)
+            {
+                cout << "Результат: " << a / b << endl;
+            }
+            else
+            {
+                cout << "Ошибка: деление на ноль" << endl;
+            }
+            break;
+        default:
+            cout << "Неправильная операция" << endl;
+    }
+    return 0;
+}
